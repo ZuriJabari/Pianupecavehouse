b2d80584bf0823ec325ea315886d902bf18b60c2
Your Name <you@example.com>
2026-02-25 10:47:41 +0300

    feat: Change footer background to legend-optimized.jpg
    Updated footer background image as requested:
    
    Footer Background:
    - Changed from camera/backgrounds/bg-interlude-02-optimized.jpg
    - Changed to images/legend-optimized.jpg
    - Maintained same opacity (70%) and positioning
    
    The footer now uses the specified legend-optimized.jpg image.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -828,7 +828,7 @@
 		<footer class="relative border-t border-black/40 bg-black">
 			<div
 				class="pointer-events-none absolute inset-0 bg-cover bg-center opacity-70"
-				style="background-image: url('{{ asset('camera/backgrounds/bg-interlude-02-optimized.jpg') }}');"
+				style="background-image: url('{{ asset('images/legend-optimized.jpg') }}');"
 			></div>
 			<div class="pointer-events-none absolute inset-0 bg-black/60"></div>
 
